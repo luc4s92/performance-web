7a814e718560083a6cb3218372e7afe75511a37d
Lucas Gonzalez <luc4s92.g@gmail.com>
2023-09-29 17:30:27 -0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+
+interface Props{
+  type: string,
+  query: string,
+  category: string
+}
+
+const Header = ({type,query, category} :Props) => {
+
+  if(query && category){
+    return (
+      <h1 className="heading3 self-star text-white-800">
+        Search results for "{query}" in <span className="capitalize">{category}</span>
+      </h1>
+    )
+  }
+
+  if(query){
+    return (
+      <h1 className="heading3 self-star text-white-800">
+        Search results for "{query}" 
+      </h1>
+    )
+  }
+  if(category){
+    return (
+      <h1 className="heading3 self-star text-white-800">
+        Search results for "{category}" 
+      </h1>
+    )
+  }
+
+  return (
+    <h1 className="heading3 self-star text-white-800">No Results</h1>
+  )
+}
+
+export default Header
